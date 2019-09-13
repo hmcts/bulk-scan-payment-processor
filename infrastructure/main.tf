d1c75fe3974a6045c84c66ea0dc19420d1688f29
nitinprabhu <prabhunitin61@gmail.com>
2019-09-13 12:07:34 +0100

    Added module and other config for infra build

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,1 +1,26 @@
 provider "azurerm" {}
+
+locals {
+  ase_name            = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  is_preview          = "${(var.env == "preview" || var.env == "spreview")}"
+  local_env           = "${local.is_preview ? "aat" : var.env}"
+  s2s_rg              = "rpe-service-auth-provider-${local.local_env}"
+  s2s_url             = "http://${local.s2s_rg}.service.core-compute-${local.local_env}.internal"
+  ccd_api_url         = "http://ccd-data-store-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
+
+  vaultName           = "bulk-scan-${var.env}"
+}
+
+module "bulk-scan-pay-processor" {
+  source              = "git@github.com:hmcts/cnp-module-webapp?ref=master"
+  product             = "${var.product}-${var.component}"
+  location            = "${var.location_app}"
+  env                 = "${var.env}"
+  ilbIp               = "${var.ilbIp}"
+  subscription        = "${var.subscription}"
+  capacity            = "${var.capacity}"
+  common_tags         = "${var.common_tags}"
+
+  app_settings = {
+  }
+}
