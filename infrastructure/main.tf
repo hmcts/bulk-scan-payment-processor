7826046a8e1df4ad1a70e2dfe7e2b3d3c940bf14
nitinprabhu <prabhunitin61@gmail.com>
2019-09-13 12:35:56 +0100

    Apply review comments

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,11 +4,6 @@ locals {
   ase_name            = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   is_preview          = "${(var.env == "preview" || var.env == "spreview")}"
   local_env           = "${local.is_preview ? "aat" : var.env}"
-  s2s_rg              = "rpe-service-auth-provider-${local.local_env}"
-  s2s_url             = "http://${local.s2s_rg}.service.core-compute-${local.local_env}.internal"
-  ccd_api_url         = "http://ccd-data-store-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
-
-  vaultName           = "bulk-scan-${var.env}"
 }
 
 module "bulk-scan-pay-processor" {
