450ec3329450c172426d5aa72d40b3f89be24896
nitinprabhu <prabhunitin61@gmail.com>
2019-09-16 19:55:09 +0100

    Added root controller test for pipeline to go green

---
+++ src/test/java/uk/gov/hmcts/reform/bulkscan.payment.processor/controllers/RootControllerTest.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.bulkscan.payment.processor.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RootController.class)
+public class RootControllerTest {
+
+    @Autowired
+    private transient MockMvc mockMvc;
+
+    @DisplayName("Should welcome upon root request with 200 response code")
+    @Test
+    void welcomeRootEndpoint() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Welcome to bulk-scan-payment-processor"));
+    }
+}
+
