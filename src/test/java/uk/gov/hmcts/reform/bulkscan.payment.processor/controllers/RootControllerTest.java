0895546a763a75512cba113fcee2e1fcfa5d6b79
nitinprabhu <prabhunitin61@gmail.com>
2019-09-16 20:17:04 +0100

    Added assert to resolve pmd issue

--- src/test/java/uk/gov/hmcts/reform/bulkscan.payment.processor/controllers/RootControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan.payment.processor/controllers/RootControllerTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RootController.class)
 public class RootControllerTest {
@@ -19,9 +19,11 @@ public class RootControllerTest {
     @DisplayName("Should welcome upon root request with 200 response code")
     @Test
     void welcomeRootEndpoint() throws Exception {
-        mockMvc.perform(get("/"))
-            .andExpect(status().isOk())
-            .andExpect(content().string("Welcome to bulk-scan-payment-processor"));
+        MockHttpServletResponse response = mockMvc.perform(get("/"))
+            .andReturn().getResponse();
+
+        assertThat(response.getContentAsString()).isEqualTo("Welcome to bulk-scan-payment-processor");
+        assertThat(response.getStatus()).isEqualTo(200);
     }
 }
 
