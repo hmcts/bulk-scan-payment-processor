7164fc193cf5a322de3c1e97a24838d42a97733a
mdayican <mustafa.dayican@hmcts.net>
2019-09-24 11:43:51 +0100

    BPS-765 queue config

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan.payment.processor/config/QueueClientsConfiguration.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.bulkscan.payment.processor.config;
+
+import com.microsoft.azure.servicebus.ClientFactory;
+import com.microsoft.azure.servicebus.IMessageReceiver;
+import com.microsoft.azure.servicebus.ReceiveMode;
+import com.microsoft.azure.servicebus.primitives.ServiceBusException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueueClientsConfiguration {
+
+    @Bean("payments-messageReceiver")
+    public IMessageReceiver envelopesMessageReceiver(
+        @Value("${azure.servicebus.payments.connection-string}") String connectionString
+    ) throws InterruptedException, ServiceBusException {
+        return ClientFactory.createMessageReceiverFromConnectionString(
+            connectionString,
+            ReceiveMode.PEEKLOCK
+        );
+    }
+
+}
