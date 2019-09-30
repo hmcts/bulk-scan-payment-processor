52d85cf5b041e578957b4800f62d825b0e0680e5
mdayican <mustafa.dayican@hmcts.net>
2019-09-30 12:44:50 +0100

    add feign client and tests

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/SwaggerPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/SwaggerPublisher.java
@@ -19,8 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Built-in feature which saves service's swagger specs in temporary directory.
  * Each travis run on master should automatically save and upload (if updated) documentation.
  */
-@SpringJUnitWebConfig
-@SpringBootTest
 @AutoConfigureMockMvc
 @IntegrationTest
 class SwaggerPublisher {
