06764a46618362ec134c569bec094004516195f4
Aliveni Choppa <aliveni.ch@gmail.com>
2019-10-01 11:25:18 +0100

    Copy payments processor s2s secret to bulkscan keyvault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,32 @@
 provider "azurerm" {
   version = "=1.22.1"
 }
+
+locals {
+  s2s_rg              = "rpe-service-auth-provider-${var.env}"
+  s2s_url             = "http://${local.s2s_rg}.service.core-compute-${var.env}.internal"
+
+  vaultName           = "bulk-scan-${var.env}"
+}
+
+data "azurerm_key_vault" "s2s_key_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "${local.s2s_rg}"
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
+  name      = "microservicekey-bulk-scan-payment-processor"
+}
+
+# Copy s2s key from shared to local vault
+data "azurerm_key_vault" "local_key_vault" {
+  name = "${local.vaultName}"
+  resource_group_name = "${local.vaultName}"
+}
+
+resource "azurerm_key_vault_secret" "local_s2s_key" {
+  name         = "microservicekey-bulk-scan-payment-processor"
+  value        = "${data.azurerm_key_vault_secret.s2s_secret.value}"
+  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+}
