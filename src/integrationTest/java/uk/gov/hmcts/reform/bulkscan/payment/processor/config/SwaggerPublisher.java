6898a625ed1df8ae4dc83e8af5101e88a0f150be
mdayican <mustafa.dayican@hmcts.net>
2019-10-01 16:40:08 +0100

    add AutoConfigureMockMvc

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/SwaggerPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/SwaggerPublisher.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.OutputStream;
