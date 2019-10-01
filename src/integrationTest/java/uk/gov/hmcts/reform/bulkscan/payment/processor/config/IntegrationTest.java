ba9a2255692a59b7abf383f7ba56571b202962b0
mdayican <mustafa.dayican@hmcts.net>
2019-10-01 14:48:11 +0100

    add/change integration test config and wiremock setting. I will use wiremock to test feign client

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/IntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/IntegrationTest.java
@@ -2,7 +2,9 @@ package uk.gov.hmcts.reform.bulkscan.payment.processor.config;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.lang.annotation.Documented;
@@ -10,7 +12,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
 /**
  * ## Integration test annotation.
  * This annotation is meant for all the integration tests.
@@ -18,10 +19,13 @@ import java.lang.annotation.Target;
  *  - centralise the integration test configuration
  *  - avoid copy and past
  */
+
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @ExtendWith(SpringExtension.class)
+@AutoConfigureWireMock
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = IntegrationContextInitializer.class)
 @ActiveProfiles({"integration","nosb"}) // no servicebus queue handler registration
 public @interface IntegrationTest {}
