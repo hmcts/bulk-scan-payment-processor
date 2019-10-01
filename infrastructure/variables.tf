4321085ac66c3b4a07c139861831ee4b73c3b17d
Aliveni Choppa <aliveni.ch@gmail.com>
2019-10-01 11:33:07 +0100

    remove unused variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,11 +1,3 @@
-variable "raw_product" {
-  default = "bulk-scan" // jenkins-library overrides product for PRs and adds e.g. pr-118-bulk-scan
-}
-
 variable "env" {
   type = "string"
-}
-
-variable "s2s_name" {
-  default = "bulk_scan_payment_processor"
 }
