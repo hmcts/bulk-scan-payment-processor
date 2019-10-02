cefc418e5c87ebaeaa5995163c5bc38ffaab60d9
Aliveni Choppa <aliveni.ch@gmail.com>
2019-10-02 08:21:31 +0100

    renamed local vault as bulk scan vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,14 +19,14 @@ data "azurerm_key_vault_secret" "s2s_secret" {
   name      = "microservicekey-bulk-scan-payment-processor"
 }
 
-# Copy s2s key from shared to local vault
-data "azurerm_key_vault" "local_key_vault" {
+# Copy s2s secret from s2s key vault to bulkscan key vault
+data "azurerm_key_vault" "bulk_scan_key_vault" {
   name = "${local.vaultName}"
   resource_group_name = "${local.vaultName}"
 }
 
-resource "azurerm_key_vault_secret" "local_s2s_key" {
+resource "azurerm_key_vault_secret" "bulk_scan_s2s_secret" {
   name         = "microservicekey-bulk-scan-payment-processor"
   value        = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+  key_vault_id = "${data.azurerm_key_vault.bulk_scan_key_vault.id}"
 }
