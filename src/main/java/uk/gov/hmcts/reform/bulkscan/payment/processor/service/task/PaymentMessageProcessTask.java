eb5b6f8f5bc14965c785e779ccab8ab3f0f3a3bc
mdayican <mustafa.dayican@hmcts.net>
2019-10-02 21:27:21 +0100

    add scheduled task

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/task/PaymentMessageProcessTask.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.bulkscan.payment.processor.service.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.PaymentMessageProcessor;
+
+@Service
+@ConditionalOnProperty(value = "scheduling.task.consume-payments-queue.enabled", matchIfMissing = true)
+public class PaymentMessageProcessTask {
+
+    private static final Logger log = LoggerFactory.getLogger(PaymentMessageProcessTask.class);
+
+    private final PaymentMessageProcessor paymentMessageProcessor;
+
+    public PaymentMessageProcessTask(
+        PaymentMessageProcessor paymentMessageProcessor) {
+        this.paymentMessageProcessor = paymentMessageProcessor;
+    }
+
+    @Scheduled(fixedDelayString = "${scheduling.task.consume-payments-queue.time-interval}" )
+    public void consumeMessages() {
+        log.info("Started the job consuming envelope messages");
+
+        try {
+            boolean queueMayHaveMessages = true;
+
+            while (queueMayHaveMessages) {
+                queueMayHaveMessages = paymentMessageProcessor.processNextMessage();
+            }
+
+            log.info("Finished the job consuming envelope messages");
+        } catch (InterruptedException exception) {
+            logTaskError(exception);
+            Thread.currentThread().interrupt();
+        } catch (Exception exception) {
+            logTaskError(exception);
+        }
+    }
+
+    private void logTaskError(Exception exception) {
+        log.error("An error occurred when running the 'consume messages' task", exception);
+    }
+}
