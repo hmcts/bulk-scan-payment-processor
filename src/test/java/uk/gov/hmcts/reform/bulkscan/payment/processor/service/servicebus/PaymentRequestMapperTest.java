b8103bffacb5af662a4ccb7d24ba81e55aa76c82
mdayican <mustafa.dayican@hmcts.net>
2019-10-03 11:07:18 +0100

    test case add for payments not found

--- src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/servicebus/PaymentRequestMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/servicebus/PaymentRequestMapperTest.java
@@ -9,6 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.PaymentRequest;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.config.SiteConfiguration;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.exception.SiteNotFoundException;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.exceptions.InvalidMessageException;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.model.PaymentMessage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -24,15 +26,12 @@ public class PaymentRequestMapperTest {
     @Mock
     private SiteConfiguration siteConfig;
 
-
     private PaymentRequestMapper paymentRequestMapper;
-
 
     @BeforeEach
     void setUp() {
         paymentRequestMapper = new PaymentRequestMapper(siteConfig);
     }
-
 
     @Test
     public void should_return_valid_PaymentRequest_when_paymentMessage_is_valid() {
@@ -45,7 +44,7 @@ public class PaymentRequestMapperTest {
             "A123"
         );
 
-        PaymentRequest  paymentRequest= paymentRequestMapper.mapPaymentMessage(paymentMessage(
+        PaymentRequest paymentRequest = paymentRequestMapper.mapPaymentMessage(paymentMessage(
             "case_number_3333",
             false
         ));
@@ -53,7 +52,6 @@ public class PaymentRequestMapperTest {
 
         assertThat(paymentRequest).isEqualToComparingFieldByFieldRecursively(expectedPaymentRequest);
     }
-
 
     @Test
     public void should_throw_SiteNotConfiguredException_when_site_not_found() {
@@ -63,8 +61,24 @@ public class PaymentRequestMapperTest {
         assertThatThrownBy(
             () -> paymentRequestMapper.mapPaymentMessage(paymentMessage("case_number_1231", true)))
             .isInstanceOf(SiteNotFoundException.class)
-            .hasMessage("Site not Found for  po box : "+PO_BOX);
+            .hasMessage("Site not Found for  po box : " + PO_BOX);
     }
 
+    @Test
+    public void should_throw_InvalidMessageException_when_no_payments_info() {
 
+        PaymentMessage paymentMessage = new PaymentMessage(
+            "envelope_id_123",
+            "case_num_32213",
+            true,
+            PO_BOX,
+            "Divorce",
+            ImmutableList.of()
+        );
+
+        assertThatThrownBy(
+            () -> paymentRequestMapper.mapPaymentMessage(paymentMessage))
+            .isInstanceOf(InvalidMessageException.class)
+            .hasMessage("No Document Control Numbers found in the payment message. MessageId: envelope_id_123");
+    }
 }
