2d9339f5bc3856e19d4b71c582294faa1a69cb95
mdayican <mustafa.dayican@hmcts.net>
2019-10-09 09:21:06 +0100

    refactor logs

--- src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/servicebus/PaymentMessageParser.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/servicebus/PaymentMessageParser.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.servicebus.MessageBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.exceptions.InvalidMessageException;
@@ -13,6 +15,8 @@ import java.util.List;
 @Service
 public class PaymentMessageParser {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PaymentMessageProcessor.class);
+
     private final ObjectMapper objectMapper;
 
     private static final String ERROR_CAUSE = "Message Binary data is null";
@@ -23,8 +27,11 @@ public class PaymentMessageParser {
 
     public PaymentMessage parse(MessageBody messageBody) {
         try {
-            return objectMapper.readValue(getBinaryData(messageBody), PaymentMessage.class);
+            PaymentMessage payment = objectMapper.readValue(getBinaryData(messageBody), PaymentMessage.class);
+            logMessageParsed(payment);
+            return payment;
         } catch (IOException exc) {
+            LOGGER.error("Payment queue message, parse error ", exc);
             throw new InvalidMessageException(exc);
         }
     }
@@ -36,6 +43,20 @@ public class PaymentMessageParser {
         }
 
         return CollectionUtils.isEmpty(binaryData) ? null : binaryData.get(0);
+    }
+
+    private void logMessageParsed(PaymentMessage payment) {
+        LOGGER.info(
+            "Parsed Payment message, Envelope ID: {}, CCD Case Number: {}, Is Exception Record: {}, Jurisdiction: {}, "
+                + "PO Box: {}, Service {}, Document Control Numbers : {}",
+            payment.envelopeId,
+            payment.ccdReference,
+            payment.isExceptionRecord,
+            payment.jurisdiction,
+            payment.poBox,
+            payment.service,
+            payment.payments
+        );
     }
 
 }
