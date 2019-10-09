d2f21b3f101881defc0a0cf1691b8f18cc429626
mdayican <mustafa.dayican@hmcts.net>
2019-10-09 16:06:43 +0100

    add update case ref handler

--- src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/servicebus/handler/PaymentMessageHandler.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/servicebus/handler/PaymentMessageHandler.java
@@ -2,13 +2,16 @@ package uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.handle
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.PayHubClient;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.CaseReferenceRequest;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.CreatePaymentRequest;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.response.CreatePaymentResponse;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.PaymentRequestMapper;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.model.PaymentMessage;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.model.UpdatePaymentMessage;
 
 @Service
 public class PaymentMessageHandler {
@@ -52,6 +55,30 @@ public class PaymentMessageHandler {
         );
 
         return paymentResult;
+    }
+
+
+    public void updatePaymentCaseReference(UpdatePaymentMessage paymentMessage) {
+
+        CaseReferenceRequest request = new CaseReferenceRequest(paymentMessage.newCaseRef);
+
+        log.info(
+            "Sending payment update case reference request, envelope id {}, case ref {}, exception ref {}",
+            paymentMessage.envelopeId,
+            request.ccdCaseNumber,
+            paymentMessage.exceptionRecordRef
+        );
+
+        ResponseEntity<?> response = payHubClient.updateCaseReference(
+            authTokenGenerator.generate(),
+            paymentMessage.exceptionRecordRef,
+            request
+        );
+
+        log.info("Payment update response from PayHub, envelope id {}, http status {} ",
+                 paymentMessage.envelopeId,
+                 response.getStatusCode());
+
     }
 
 }
