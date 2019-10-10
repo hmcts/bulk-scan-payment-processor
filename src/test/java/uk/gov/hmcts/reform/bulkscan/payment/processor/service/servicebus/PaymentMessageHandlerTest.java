cbe9c790b952d7e9b1a7f29578126549736adf2b
mdayican <mustafa.dayican@hmcts.net>
2019-10-10 12:38:22 +0100

    Add updatepayment handler (#64)
    * add update case ref handler

--- src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/servicebus/PaymentMessageHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/servicebus/PaymentMessageHandlerTest.java
@@ -3,23 +3,31 @@ package uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.PayHubClient;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.PayHubClientException;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.CaseReferenceRequest;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.CreatePaymentRequest;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.response.CreatePaymentResponse;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.data.producer.SamplePaymentMessageData;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.handler.PaymentMessageHandler;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.model.PaymentMessage;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.model.UpdatePaymentMessage;
 
 import java.util.Optional;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -59,5 +67,59 @@ public class PaymentMessageHandlerTest {
         // then
         assertThat(result).isNotNull();
         assertThat(result.paymentDcns).hasSize(1).contains("1234");
+    }
+
+    @Test
+    public void should_call_payhub_api_to_assign_case_ref() {
+        // given
+        UpdatePaymentMessage message = new UpdatePaymentMessage(
+            "env-id-12321",
+            "Divorece",
+            "Finrem",
+            "exp-21321",
+            "cas-ref-9999"
+        );
+
+        when(s2sTokenGenerator.generate()).thenReturn("test-service");
+
+        when(payHubClient.updateCaseReference(
+            eq("test-service"), eq("exp-21321"), any(CaseReferenceRequest.class))
+        ).thenReturn(ResponseEntity.status(200).build());
+
+        // when
+        messageHandler.updatePaymentCaseReference(message);
+
+        // then
+        ArgumentCaptor<CaseReferenceRequest> requestCaptor = ArgumentCaptor.forClass(CaseReferenceRequest.class);
+
+        verify(payHubClient).updateCaseReference(any(), any(), requestCaptor.capture());
+        CaseReferenceRequest req = requestCaptor.getValue();
+        CaseReferenceRequest expectedRequest = new CaseReferenceRequest("cas-ref-9999");
+        assertThat(req).isEqualToComparingFieldByFieldRecursively(expectedRequest);
+    }
+
+
+    @Test
+    public void should_throw_exception_if_payhub_api_assign_case_ref_fails() {
+        // given
+        UpdatePaymentMessage message = new UpdatePaymentMessage(
+            "env-id-12321",
+            "Divorece",
+            "Finrem",
+            "exp-21321",
+            "cas-ref-9999"
+        );
+
+        when(s2sTokenGenerator.generate()).thenReturn("test-service");
+
+        when(payHubClient.updateCaseReference(
+            eq("test-service"), eq("exp-21321"), any(CaseReferenceRequest.class))
+        ).thenThrow(new PayHubClientException(new HttpClientErrorException(HttpStatus.BAD_REQUEST, "BAD_REQUEST")));
+
+        // when
+        assertThatThrownBy(
+            () -> messageHandler.updatePaymentCaseReference(message))
+            .isInstanceOf(PayHubClientException.class);
+
     }
 }
