61acf19937df118b24fb5d0c5bc08b879f08753e
mdayican <mustafa.dayican@hmcts.net>
2019-10-17 15:47:59 +0100

    remove  service from update payment as orchestrator can not provide

--- src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/servicebus/model/UpdatePaymentMessage.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/servicebus/model/UpdatePaymentMessage.java
@@ -6,20 +6,17 @@ public class UpdatePaymentMessage {
 
     public final String envelopeId;
     public final String jurisdiction;
-    public final String service;
     public final String exceptionRecordRef;
     public final String newCaseRef;
 
     public UpdatePaymentMessage(
         @JsonProperty(value = "envelope_id", required = true) String envelopeId,
         @JsonProperty(value = "jurisdiction", required = true) String jurisdiction,
-        @JsonProperty(value = "service", required = true) String service,
         @JsonProperty(value = "exception_record_ref", required = true) String exceptionRecordRef,
         @JsonProperty(value = "new_case_ref", required = true) String newCaseRef
     ) {
         this.envelopeId = envelopeId;
         this.jurisdiction = jurisdiction;
-        this.service = service;
         this.exceptionRecordRef = exceptionRecordRef;
         this.newCaseRef = newCaseRef;
     }
