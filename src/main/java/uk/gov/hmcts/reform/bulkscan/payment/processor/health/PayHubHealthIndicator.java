03e1100319059e4d3e3dec5d7abb6b438ae094e9
aliveni <aliveni.choppa@hmcts.net>
2019-10-24 15:32:49 +0100

    Add smoke tests (#103)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/health/PayHubHealthIndicator.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.bulkscan.payment.processor.health;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class PayHubHealthIndicator implements HealthIndicator {
+
+    private static final Logger log = LoggerFactory.getLogger(PayHubHealthIndicator.class);
+
+    private final String payHubUri;
+    private final RestTemplate restTemplate;
+
+    public PayHubHealthIndicator(
+        @Value("${pay-hub.api.url}") String payHubUri,
+        RestTemplate restTemplate
+    ) {
+        this.payHubUri = payHubUri;
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public Health health() {
+        try {
+            ResponseEntity<Object> response = restTemplate.getForEntity(payHubUri + "/health", Object.class);
+            return response.getStatusCode() == (HttpStatus.OK) ? statusHealthy() : statusDown();
+        } catch (Exception ex) {
+            log.error("Exception occurred while checking Pay hub health", ex);
+            return statusUnknown(ex);
+        }
+    }
+
+    private Health statusHealthy() {
+        return Health.up().build();
+    }
+
+    private Health statusDown() {
+        return Health.down().build();
+    }
+
+    private Health statusUnknown(Throwable ex) {
+        return Health.unknown().withException(ex).build();
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/health/QueueHealthIndicator.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.reform.bulkscan.payment.processor.health;
+
+import com.microsoft.azure.servicebus.IMessageReceiver;
+import com.microsoft.azure.servicebus.primitives.ServiceBusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueueHealthIndicator implements HealthIndicator {
+    private static final Logger log = LoggerFactory.getLogger(QueueHealthIndicator.class);
+
+    private final IMessageReceiver messageReceiver;
+
+    public QueueHealthIndicator(IMessageReceiver messageReceiver) {
+        this.messageReceiver = messageReceiver;
+    }
+
+    @Override
+    public Health health() {
+        try {
+            messageReceiver.peek();
+            return Health.up().build();
+        } catch (InterruptedException e) {
+            log.error("Error occurred while reading messages from payments queue", e);
+            Thread.currentThread().interrupt();
+            return Health.down().withException(e).build();
+        } catch (ServiceBusException e) {
+            log.error("Error occurred while reading messages from payments queue", e);
+            return Health.down().withException(e).build();
+        }
+    }
+}
