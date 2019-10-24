91b7641deb26aa8165d410fb6ef758895e965279
Aliveni Choppa <aliveni.ch@gmail.com>
2019-10-24 08:50:39 +0100

    Update the existing smoke test to check the status

--- src/smokeTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/PaymentProcessorHealthTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/PaymentProcessorHealthTest.java
@@ -2,28 +2,29 @@ package uk.gov.hmcts.reform.bulkscan.payment.processor;
 
 import com.typesafe.config.ConfigFactory;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @TestPropertySource("classpath:application.conf")
-public class DummySmokeTest {
+class PaymentProcessorHealthTest {
 
     private static final String TEST_URL = ConfigFactory.load().getString("test-url");
 
     @Test
-    public void replaceThisWithActualTests() {
-        // TODO: this test is there so that a test report can be created for smoke tests
-        // (otherwise the build fails). Remove when actual smoke tests have been written.
-        Response response = RestAssured
+    void payment_processor_is_healthy() {
+        RestAssured
             .given()
             .relaxedHTTPSValidation()
             .baseUri(TEST_URL)
             .get("/health")
-            .andReturn();
-
-        assertThat(response.getStatusCode()).isEqualTo(200);
+            .then()
+            .assertThat()
+            .statusCode(HttpStatus.OK.value())
+            .and()
+            .body("status", equalTo(Status.UP.toString()));
     }
 }
