30810e171f3c9e2c9b786c97e1a0a5ac68c71e18
apereverzin <andrey.pereverzin@hotmail.com>
2019-10-30 17:32:12 +0000

    WIP

--- src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/QueueClientsConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/QueueClientsConfiguration.java
@@ -4,6 +4,8 @@ import com.microsoft.azure.servicebus.ClientFactory;
 import com.microsoft.azure.servicebus.IMessageReceiver;
 import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +14,13 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("!nosb") // do not register handler for the nosb (test) profile
 public class QueueClientsConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(QueueClientsConfiguration.class);
 
     @Bean
     public IMessageReceiver paymentMessageReceiver(
         @Value("${azure.servicebus.payments.connection-string}") String connectionString
     ) throws InterruptedException, ServiceBusException {
+        log.info("connection-string: " + connectionString);
         return ClientFactory.createMessageReceiverFromConnectionString(
             connectionString,
             ReceiveMode.PEEKLOCK
