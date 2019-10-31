108df91a84cad388aa7424ac7b6434173ab76a88
apereverzin <andrey.pereverzin@hotmail.com>
2019-10-31 12:43:44 +0000

    WIP

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/FunctionalQueueConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/FunctionalQueueConfig.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.bulkscan.payment.processor;
 
+import com.microsoft.azure.servicebus.IMessageReceiver;
 import com.microsoft.azure.servicebus.QueueClient;
 import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
@@ -7,6 +8,8 @@ import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+
+import static org.mockito.Mockito.mock;
 
 public class FunctionalQueueConfig {
 
@@ -20,5 +23,11 @@ public class FunctionalQueueConfig {
             new ConnectionStringBuilder(paymentsQueueWriteConnectionString),
             ReceiveMode.PEEKLOCK
         );
+    }
+
+    @Bean
+    @Profile("nosb") // apply only when Service Bus should not be used
+    public IMessageReceiver testMessageReceiver() {
+        return mock(IMessageReceiver.class);
     }
 }
