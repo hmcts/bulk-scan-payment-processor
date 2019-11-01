5f96dbd04d3ee3d9dd6ccc40d4d05ed879a9543f
apereverzin <andrey.pereverzin@hotmail.com>
2019-11-01 17:20:06 +0000

    Added !nosb

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/FunctionalQueueConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/FunctionalQueueConfig.java
@@ -6,15 +6,8 @@ import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.ResponseEntity;
-import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.PayHubClient;
-import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.CaseReferenceRequest;
-import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.CreatePaymentRequest;
-import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.response.CreatePaymentResponse;
 
 import static org.mockito.Mockito.mock;
 
@@ -36,39 +29,5 @@ public class FunctionalQueueConfig {
     @Profile("nosb") // apply only when Service Bus should not be used
     public IMessageReceiver testMessageReceiver() {
         return mock(IMessageReceiver.class);
-    }
-
-    @Bean
-    @Profile("nosb") // apply only when Service Bus should not be used
-    PayHubClient payHubClient() {
-        return new PayHubClient() {
-            @Override
-            public ResponseEntity<CreatePaymentResponse> createPayment(
-                String serviceAuthorisation,
-                CreatePaymentRequest paymentRequest
-            ) {
-                return null;
-            }
-
-            @Override
-            public ResponseEntity<Void> updateCaseReference(
-                String serviceAuthorisation,
-                String exceptionReference,
-                CaseReferenceRequest caseReferenceRequest
-            ) {
-                return null;
-            }
-        };
-    }
-
-    @Bean
-    @Profile("nosb") // apply only when Service Bus should not be used
-    HealthIndicator healthIndicator() {
-        return new HealthIndicator() {
-            @Override
-            public Health health() {
-                return null;
-            }
-        };
     }
 }
