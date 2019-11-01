20214d8f0fab34234678aad258edf1dc35245fe7
leszekg <gonczar@gmail.com>
2019-11-01 00:19:21 +0000

    WIP

--- src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/ccd/CcdClient.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/ccd/CcdClient.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.bulkscan.payment.processor.ccd;
 
 import com.google.common.collect.ImmutableMap;
+import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -43,30 +44,35 @@ public class CcdClient {
         String caseTypeId = service.toUpperCase() + "_ExceptionRecord";
         String eventId = COMPLETE_AWAITING_DCN_PROCESSING_EVENT_ID;
 
-        StartEventResponse startEventResponse =
-            startEvent(authenticator, jurisdiction, caseTypeId, exceptionRecordCcdId, eventId);
+        try {
+            StartEventResponse startEventResponse =
+                startEvent(authenticator, jurisdiction, caseTypeId, exceptionRecordCcdId, eventId);
 
-        Event event = Event
-            .builder()
-            .summary("Complete payment DCN processing")
-            .id(startEventResponse.getEventId())
-            .build();
+            Event event = Event
+                .builder()
+                .summary("Complete payment DCN processing")
+                .id(startEventResponse.getEventId())
+                .build();
 
-        CaseDataContent caseDataContent = CaseDataContent.builder()
-            .data(ImmutableMap.of(AWAITING_DCN_PROCESSING_FIELD_NAME, "No"))
-            .event(event)
-            .ignoreWarning(true)
-            .caseReference(exceptionRecordCcdId)
-            .eventToken(startEventResponse.getToken())
-            .build();
+            CaseDataContent caseDataContent = CaseDataContent.builder()
+                .data(ImmutableMap.of(AWAITING_DCN_PROCESSING_FIELD_NAME, "No"))
+                .event(event)
+                .ignoreWarning(true)
+                .caseReference(exceptionRecordCcdId)
+                .eventToken(startEventResponse.getToken())
+                .build();
 
-        submitEvent(authenticator, jurisdiction, caseTypeId, exceptionRecordCcdId, caseDataContent);
+            submitEvent(authenticator, jurisdiction, caseTypeId, exceptionRecordCcdId, caseDataContent);
 
-        log.info(
-            "Completed awaiting payment DCN processing. Exception record ID: {}, service {}",
-            exceptionRecordCcdId,
-            service
-        );
+            log.info(
+                "Completed awaiting payment DCN processing. Exception record ID: {}, service {}",
+                exceptionRecordCcdId,
+                service
+            );
+        } catch (FeignException ex) {
+            log.error("Feign exception. Body: {}", ex.contentUTF8(), ex);
+            throw ex;
+        }
     }
 
     private StartEventResponse startEvent(
@@ -76,6 +82,15 @@ public class CcdClient {
         String caseRef,
         String eventTypeId
     ) {
+        log.info(
+            "Starting event for user {}, jurisdiction {}, case type {}, case {}, event {}",
+            authenticator.getUserDetails().getEmail(),
+            jurisdiction,
+            caseTypeId,
+            caseRef,
+            eventTypeId
+        );
+
         StartEventResponse response = ccdApi.startEventForCaseWorker(
             authenticator.getUserToken(),
             authenticator.getServiceToken(),
