b53366d0cf550647291cbba761350f137f28f39d
apereverzin <andrey.pereverzin@hotmail.com>
2019-11-01 16:39:37 +0000

    Removed PAY_HUB_URL

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/FunctionalQueueConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/FunctionalQueueConfig.java
@@ -8,6 +8,11 @@ import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.PayHubClient;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.CaseReferenceRequest;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.CreatePaymentRequest;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.response.CreatePaymentResponse;
 
 import static org.mockito.Mockito.mock;
 
@@ -29,5 +34,21 @@ public class FunctionalQueueConfig {
     @Profile("nosb") // apply only when Service Bus should not be used
     public IMessageReceiver testMessageReceiver() {
         return mock(IMessageReceiver.class);
+    }
+
+    @Bean
+    @Profile("nosb") // apply only when Service Bus should not be used
+    PayHubClient payHubClient() {
+        return new PayHubClient() {
+            @Override
+            public ResponseEntity<CreatePaymentResponse> createPayment(String serviceAuthorisation, CreatePaymentRequest paymentRequest) {
+                return null;
+            }
+
+            @Override
+            public ResponseEntity<Void> updateCaseReference(String serviceAuthorisation, String exceptionReference, CaseReferenceRequest caseReferenceRequest) {
+                return null;
+            }
+        };
     }
 }
