b97a64096f4eacfd52a657ddac250ebb82cf337c
mdayican <mustafa.dayican@hmcts.net>
2020-04-22 12:29:08 +0100

    disable func test

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/PaymentForExceptionRecordTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/PaymentForExceptionRecordTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.bulkscan.payment.processor;
 
 import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +24,7 @@ import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 @ActiveProfiles("functional")
+@Disabled
 class PaymentForExceptionRecordTest {
 
     private static final String AWAITING_DNC_PROCESSING_FLAG_NAME = "awaitingPaymentDCNProcessing";
