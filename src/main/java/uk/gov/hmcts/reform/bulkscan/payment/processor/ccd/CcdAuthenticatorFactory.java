27917f837e87137c1d2367dce0ac8f0a73368362
mdayican <mustafa.dayican@hmcts.net>
2020-05-20 13:37:36 +0100

    log for testing

--- src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/ccd/CcdAuthenticatorFactory.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/ccd/CcdAuthenticatorFactory.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.bulkscan.payment.processor.ccd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
@@ -14,6 +16,7 @@ public class CcdAuthenticatorFactory {
     private final AuthTokenGenerator s2sTokenGenerator;
     private final IdamClient idamClient;
     private final JurisdictionToUserMapping users;
+    private static final Logger log = LoggerFactory.getLogger(CcdAuthenticatorFactory.class);
 
     public CcdAuthenticatorFactory(
         AuthTokenGenerator s2sTokenGenerator,
@@ -27,9 +30,10 @@ public class CcdAuthenticatorFactory {
 
     public CcdAuthenticator createForJurisdiction(String jurisdiction) {
         Credential user = users.getUser(jurisdiction);
+        log.info("getting Idam Open Id Access token ");
         String userToken = idamClient.getAccessToken(user.getUsername(), user.getPassword());
-        UserDetails userDetails = idamClient.getUserDetails(userToken);
 
+        UserDetails userDetails = idamClient.getUserDetails(userToken);
         return new CcdAuthenticator(
             s2sTokenGenerator::generate,
             userDetails,
