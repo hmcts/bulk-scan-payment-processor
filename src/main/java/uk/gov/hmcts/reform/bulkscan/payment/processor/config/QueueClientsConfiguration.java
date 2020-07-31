6dbccbba249dfe4c4e921481e9933b5f5b914608
Donatas Martinkus <doncem@users.noreply.github.com>
2020-07-31 13:17:34 +0100

    Replace connection string with access key and namespace

--- src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/QueueClientsConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/QueueClientsConfiguration.java
@@ -3,8 +3,9 @@ package uk.gov.hmcts.reform.bulkscan.payment.processor.config;
 import com.microsoft.azure.servicebus.ClientFactory;
 import com.microsoft.azure.servicebus.IMessageReceiver;
 import com.microsoft.azure.servicebus.ReceiveMode;
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,13 +15,62 @@ import org.springframework.context.annotation.Profile;
 public class QueueClientsConfiguration {
 
     @Bean
+    @ConfigurationProperties(prefix = "azure.servicebus.payments")
+    private QueueConfig paymentQueueConfig() {
+        return new QueueConfig();
+    }
+
+    @Bean
     public IMessageReceiver paymentMessageReceiver(
-        @Value("${azure.servicebus.payments.connection-string}") String connectionString
+        QueueConfig queueConfig
     ) throws InterruptedException, ServiceBusException {
-        return ClientFactory.createMessageReceiverFromConnectionString(
-            connectionString,
+        return ClientFactory.createMessageReceiverFromConnectionStringBuilder(
+            new ConnectionStringBuilder(
+                queueConfig.getNamespace(),
+                queueConfig.getQueueName(),
+                queueConfig.getAccessKeyName(),
+                queueConfig.getAccessKey()
+            ),
             ReceiveMode.PEEKLOCK
         );
     }
 
+    private static class QueueConfig {
+        private String accessKey;
+        private String accessKeyName;
+        private String namespace;
+        private String queueName;
+
+        public String getAccessKey() {
+            return accessKey;
+        }
+
+        public String getAccessKeyName() {
+            return accessKeyName;
+        }
+
+        public String getNamespace() {
+            return namespace;
+        }
+
+        public String getQueueName() {
+            return queueName;
+        }
+
+        public void setAccessKey(String accessKey) {
+            this.accessKey = accessKey;
+        }
+
+        public void setAccessKeyName(String accessKeyName) {
+            this.accessKeyName = accessKeyName;
+        }
+
+        public void setNamespace(String namespace) {
+            this.namespace = namespace;
+        }
+
+        public void setQueueName(String queueName) {
+            this.queueName = queueName;
+        }
+    }
 }
