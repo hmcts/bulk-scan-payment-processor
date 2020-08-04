ae811293afe41dd5f71045b14bf1c47bfd982be9
donatas M <1420191+doncem@users.noreply.github.com>
2020-08-04 10:56:20 +0100

    Replace connection string with access key and namespace (#264)

--- src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/QueueClientsConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/QueueClientsConfiguration.java
@@ -3,8 +3,9 @@ package uk.gov.hmcts.reform.bulkscan.payment.processor.config;
 import com.microsoft.azure.servicebus.ClientFactory;
 import com.microsoft.azure.servicebus.IMessageReceiver;
 import com.microsoft.azure.servicebus.ReceiveMode;
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,13 +15,23 @@ import org.springframework.context.annotation.Profile;
 public class QueueClientsConfiguration {
 
     @Bean
-    public IMessageReceiver paymentMessageReceiver(
-        @Value("${azure.servicebus.payments.connection-string}") String connectionString
-    ) throws InterruptedException, ServiceBusException {
-        return ClientFactory.createMessageReceiverFromConnectionString(
-            connectionString,
-            ReceiveMode.PEEKLOCK
-        );
+    @ConfigurationProperties(prefix = "azure.servicebus.payments")
+    protected QueueConfigurationProperties paymentQueueConfig() {
+        return new QueueConfigurationProperties();
     }
 
+    @Bean
+    public IMessageReceiver paymentMessageReceiver(
+        QueueConfigurationProperties queueProperties
+    ) throws InterruptedException, ServiceBusException {
+        // looks like `from connection string builder` is not properly implemented and does not include auth
+        var builder = new ConnectionStringBuilder(
+            queueProperties.getNamespace(),
+            queueProperties.getQueueName(),
+            queueProperties.getAccessKeyName(),
+            queueProperties.getAccessKey()
+        );
+
+        return ClientFactory.createMessageReceiverFromConnectionString(builder.toString(), ReceiveMode.PEEKLOCK);
+    }
 }
