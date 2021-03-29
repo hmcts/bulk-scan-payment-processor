44eb8eecaaea287ff9ce55925d0e49c7262b7be3
SomeshRA <somesh.acharya1@hmcts.net>
2021-03-29 15:47:32 +0100

    DTSBPS-451: Improving test coverage

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/client/processor/BulkScanProcessorApiProxyTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/client/processor/BulkScanProcessorApiProxyTest.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.client.processor.request.PaymentRequest;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.config.IntegrationTest;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.model.PaymentInfo;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.util.TestUtil;
 
+import java.io.IOException;
 import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
@@ -18,6 +20,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static java.util.List.of;
 import static java.util.UUID.randomUUID;
 
 @IntegrationTest
@@ -29,13 +32,17 @@ public class BulkScanProcessorApiProxyTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void should_return_ok_when_payment_is_updated() throws JsonProcessingException {
-        List<PaymentInfo> paymentInfoList = List.of(new PaymentInfo("11234"),
-            new PaymentInfo("22234"), new PaymentInfo("33234"));
+    void should_return_ok_when_payment_is_updated() throws IOException {
+        List<PaymentInfo> paymentInfoList = of(
+            new PaymentInfo("11234"),
+            new PaymentInfo("22234"),
+            new PaymentInfo("33234")
+        );
 
         PaymentRequest request = new PaymentRequest(paymentInfoList);
 
-        String requestJson = objectMapper.writeValueAsString(request);
+        String requestJson = TestUtil.fileContentAsString("testdata.post-processor/payments.json");
+
         String s2sToken = randomUUID().toString();
         stubFor(put(urlEqualTo("/payment/status"))
             .withHeader("ServiceAuthorization", equalTo(s2sToken))
