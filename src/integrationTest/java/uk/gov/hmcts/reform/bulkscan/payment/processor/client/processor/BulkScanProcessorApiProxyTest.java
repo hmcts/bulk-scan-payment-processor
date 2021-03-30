55af44934c04fa2946ab47accc40cef9d9f4a823
SomeshRA <69344273+SomeshRA@users.noreply.github.com>
2021-03-30 15:35:03 +0100

    DTSBPS-451: Adding processor client (#405)
    * DTSBPS-451: Adding processor client
    
    * DTSBPS-451: Improving test coverage
    
    * DTSBPS-451: Improving test coverage
    
    * DTSBPS-451: Improving test coverage
    
    * DTSBPS-451: Improving test coverage
    
    * DTSBPS-451: Improving test coverage
    
    * DTSBPS-451: Improving test coverage
    
    * DTSBPS-451: Improving test coverage
    
    * DTSBPS-451: Improving test coverage and implementing review comments

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/client/processor/BulkScanProcessorApiProxyTest.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.reform.bulkscan.payment.processor.client.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.client.processor.request.PaymentRequest;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.config.IntegrationTest;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.model.PaymentInfo;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.util.TestUtil;
+
+import java.io.IOException;
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.put;
+import static com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static java.util.List.of;
+import static java.util.UUID.randomUUID;
+
+@IntegrationTest
+public class BulkScanProcessorApiProxyTest {
+    @Autowired
+    private BulkScanProcessorApiProxy proxy;
+
+    @Test
+    void should_return_ok_when_payment_is_updated() throws IOException {
+        List<PaymentInfo> paymentInfoList = of(
+            new PaymentInfo("11234"),
+            new PaymentInfo("22234"),
+            new PaymentInfo("33234")
+        );
+
+        PaymentRequest request = new PaymentRequest(paymentInfoList);
+
+        String requestJson = TestUtil.fileContentAsString("testdata.post-processor/payments.json");
+
+        String s2sToken = randomUUID().toString();
+        stubFor(put(urlEqualTo("/payment/status"))
+            .withHeader("ServiceAuthorization", equalTo(s2sToken))
+            .withRequestBody(equalToJson(requestJson))
+            .willReturn(aResponse().withStatus(200)));
+
+        proxy.updateStatus(s2sToken, request);
+
+        verify(1, putRequestedFor(urlEqualTo("/payment/status"))
+            .withRequestBody(equalToJson(requestJson)));
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/client/processor/CustomFeignErrorDecoderTest.java
@@ -0,0 +1,156 @@
+package uk.gov.hmcts.reform.bulkscan.payment.processor.client.processor;
+
+import feign.FeignException;
+import feign.Request;
+import feign.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+
+@ExtendWith(MockitoExtension.class)
+class CustomFeignErrorDecoderTest {
+
+    private static final CustomFeignErrorDecoder DECODER = new CustomFeignErrorDecoder();
+
+    private static final Request REQUEST = Request.create(
+        Request.HttpMethod.GET,
+        "localhost",
+        Collections.emptyMap(),
+        Request.Body.create(new byte[0]),
+        null
+    );
+
+    @DisplayName("Should parse response and return Client specific exception")
+    @Test
+    void should_throw_client_exception() {
+        Response response = Response.builder()
+            .request(REQUEST)
+            .headers(Collections.singletonMap("AcceptTest", Collections.singletonList("Yes")))
+            .status(HttpStatus.NOT_FOUND.value())
+            .reason("Could not find")
+            .body("some body".getBytes())
+            .build();
+
+        assertThat(decode(response))
+            .isInstanceOf(HttpClientErrorException.class)
+            .hasMessage(HttpStatus.NOT_FOUND.value() + " Could not find");
+    }
+
+    @DisplayName("Should parse response and return Server specific exception")
+    @Test
+    void should_throw_server_exception() {
+        Response response = Response.builder()
+            .request(REQUEST)
+            .headers(Collections.emptyMap())
+            .status(INTERNAL_SERVER_ERROR.value())
+            .body("some body".getBytes())
+            .build();
+
+        assertThat(decode(response))
+            .isInstanceOf(HttpServerErrorException.class)
+            .hasMessage(INTERNAL_SERVER_ERROR.value() + " " + INTERNAL_SERVER_ERROR.getReasonPhrase());
+    }
+
+    @DisplayName("Should fail to parse body and throw RuntimeException instead")
+    @Test
+    void should_throw_failing_body_parsing() throws IOException {
+        Response.Body body = mock(Response.Body.class);
+        Response response = Response.builder()
+            .request(REQUEST)
+            .headers(Collections.emptyMap())
+            .status(HttpStatus.BAD_REQUEST.value())
+            .reason("bad")
+            .body(body)
+            .build();
+
+        when(body.asInputStream()).thenThrow(IOException.class);
+
+        assertThat(decode(response))
+            .isInstanceOf(RuntimeException.class)
+            .hasMessage("Failed to process response body.")
+            .hasCauseInstanceOf(IOException.class);
+    }
+
+    @DisplayName("Should decode valid response in case somehow it got in the process")
+    @Test
+    void should_throw_feign_exception() {
+        Response response = Response.builder()
+            .request(REQUEST)
+            .headers(Collections.emptyMap())
+            .status(HttpStatus.TEMPORARY_REDIRECT.value())
+            .reason("nope")
+            .body("grumps".getBytes())
+            .build();
+
+        assertThat(decode(response))
+            .isInstanceOf(FeignException.class)
+            .hasMessage("[" + HttpStatus.TEMPORARY_REDIRECT.value()
+                            + " nope] during [GET] to [localhost] [methodKey]: [grumps]");
+    }
+
+    @DisplayName("Should decode when response body is not present")
+    @Test
+    void should_handle_null_response() {
+        Request.Body body = null;
+        Request.create(
+            Request.HttpMethod.GET,
+            "localhost",
+            Collections.emptyMap(),
+            body,
+            null
+        );
+        Response response = Response.builder()
+            .request(REQUEST)
+            .headers(Collections.emptyMap())
+            .status(HttpStatus.TEMPORARY_REDIRECT.value())
+            .reason("nope")
+            .build();
+
+        assertThat(decode(response))
+            .isInstanceOf(FeignException.class)
+            .hasMessage("[" + HttpStatus.TEMPORARY_REDIRECT.value()
+                            + " nope] during [GET] to [localhost] [methodKey]: []");
+    }
+
+    @DisplayName("Should decode when response body empty")
+    @Test
+    void should_handle_empty_response() {
+        Request.create(
+            Request.HttpMethod.GET,
+            "localhost",
+            Collections.emptyMap(),
+            Request.Body.empty(),
+            null
+        );
+
+        Response response = Response.builder()
+            .request(REQUEST)
+            .headers(Collections.emptyMap())
+            .status(HttpStatus.TEMPORARY_REDIRECT.value())
+            .body("".getBytes())
+            .reason("nope")
+            .build();
+
+        assertThat(decode(response))
+            .isInstanceOf(FeignException.class)
+            .hasMessage("[" + HttpStatus.TEMPORARY_REDIRECT.value()
+                            + " nope] during [GET] to [localhost] [methodKey]: []");
+    }
+
+    private Exception decode(Response response) {
+        return DECODER.decode("methodKey", response);
+    }
+}
