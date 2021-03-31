fd5232683eaf17c8e83ab98b5738e493dc51cf90
SomeshRA <69344273+SomeshRA@users.noreply.github.com>
2021-03-31 14:38:03 +0100

    Dtsbps 451 v3 - Configuration for Async and Retry invocation of bulk scan processor end point (#409)
    * DTSBPS-451: Configuration for Async and Retry
    
    * DTSBPS-451: Configuration for Async and Retry
    
    * DTSBPS-451: Configuration for Async and Retry
    
    * DTSBPS-451: Configuration for Async and Retry

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/RetryConfig.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.bulkscan.payment.processor.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.web.client.HttpServerErrorException;
+
+import static org.springframework.retry.backoff.ExponentialBackOffPolicy.DEFAULT_MAX_INTERVAL;
+import static org.springframework.retry.backoff.ExponentialBackOffPolicy.DEFAULT_MULTIPLIER;
+
+@Configuration
+public class RetryConfig {
+
+    @Bean("RetryTemplate")
+    public RetryTemplate retryTemplate(@Value("${bulk-scan-procesor.api.retries}") int numberOfRetries,
+                                       @Value("${bulk-scan-procesor.api.wait-time-in-ms}") long timeToWait) {
+        return RetryTemplate.builder()
+                .retryOn(HttpServerErrorException.class)
+                .maxAttempts(numberOfRetries)
+                .exponentialBackoff(timeToWait, DEFAULT_MULTIPLIER, DEFAULT_MAX_INTERVAL)
+                .build();
+    }
+}
