6fb6b19def574939a68bf8e1bc8a663a5725e21a
Aliveni Choppa <Aliveni.choppa1@hmcts.net>
2023-01-11 17:13:27 +0000

    Test multiple POBoxes configuration for a site id

--- src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/SiteConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/config/SiteConfigurationTest.java
@@ -19,6 +19,7 @@ class SiteConfigurationTest {
         siteConfiguration.setSites(
             ImmutableList.of(
                 new SiteConfiguration.Sites("site_a", "12345", "99"),
+                new SiteConfiguration.Sites("site_a", "12344", "99"),
                 new SiteConfiguration.Sites("site_b", "11111", "11"),
                 new SiteConfiguration.Sites("site_c", "98756", "44")
             )
@@ -27,6 +28,7 @@ class SiteConfigurationTest {
         ReflectionTestUtils.invokeMethod(siteConfiguration, "mapPoBoxToSiteId");
 
         assertThat(siteConfiguration.getSiteIdByPoBox("12345")).isEqualTo("99");
+        assertThat(siteConfiguration.getSiteIdByPoBox("12344")).isEqualTo("99");
         assertThat(siteConfiguration.getSiteIdByPoBox("11111")).isEqualTo("11");
         assertThat(siteConfiguration.getSiteIdByPoBox("98756")).isEqualTo("44");
         assertThat(siteConfiguration.getSiteIdByPoBox("80000")).isNull();
