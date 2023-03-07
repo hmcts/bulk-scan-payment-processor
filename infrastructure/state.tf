e2a4dfb78f8af7029c082fe95141a2202d5856b0
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2023-03-07 14:22:34 +0000

    bump version

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 3.33.0"
+      version = "~> 3.46.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.35.0"
+      version = "2.36.0"
     }
   }
 }
