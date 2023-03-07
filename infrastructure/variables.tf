006fbe6bcf52f346ad5629009fd237cf427cf126
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2023-03-07 14:12:33 +0000

    remove quotes in variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -40,7 +40,7 @@ variable "idam_client_redirect_uri" {
 }
 
 variable "supported_services" {
-  type        = "list"
+  type        = list
   description = "Services to be supported by Bulk Scan in the given environment. Bulk Scan will only be able to map these ones to IDAM user credentials"
   default     = ["SSCS", "BULKSCAN", "PROBATE", "DIVORCE", "FINREM", "CMC"]
 }
