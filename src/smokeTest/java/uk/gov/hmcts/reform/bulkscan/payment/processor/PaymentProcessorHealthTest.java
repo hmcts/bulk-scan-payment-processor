a939b2b3add4208a7dd1534ae74acb5efb3bc9a0
Imran Ali <Imran.Ali@justice.gov.uk>
2023-10-26 00:34:14 +0100

    smoke test fix

--- src/smokeTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/PaymentProcessorHealthTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/bulkscan/payment/processor/PaymentProcessorHealthTest.java
@@ -2,14 +2,17 @@ package uk.gov.hmcts.reform.bulkscan.payment.processor;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.hamcrest.Matchers.equalTo;
 
 @TestPropertySource("classpath:application.properties")
+@ExtendWith(SpringExtension.class)
 class PaymentProcessorHealthTest {
 
     @Value("${test-url}")
