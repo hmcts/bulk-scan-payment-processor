2857646739a69ec83180c23378c1ebae22a203c6
Josh Blackmoor <joshblackmoor01@icloud.com>
2024-08-22 19:38:34 +0100

    Added more unit tests

--- src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/PaymentHubHandlerServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/PaymentHubHandlerServiceTest.java
@@ -16,9 +16,13 @@ import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.Crea
 import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.response.CreatePaymentResponse;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.data.producer.SamplePaymentMessageData;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.errorhandling.exception.PayHubCallException;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.models.CreatePayment;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.models.PaymentInfo;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.models.UpdatePayment;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.model.CreatePaymentMessage;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.model.UpdatePaymentMessage;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static java.util.Collections.singletonList;
@@ -301,5 +305,167 @@ class PaymentHubHandlerServiceTest {
                     + "Envelope id: env-id-12321, Exception record ref: exp-21321, New case ref: cas-ref-9999"
             );
         assertThat(exception.getCause()).isEqualTo(mockFeignException);
+    }
+
+
+    @Test
+    void should_call_payhub_api_to_update_payment_case_reference() {
+        UpdatePayment updatePayment = new UpdatePayment(
+            "env-id-12321",
+            "Divorce",
+            "exp-21321",
+            "cas-ref-9999"
+        );
+
+        when(s2sTokenGenerator.generate()).thenReturn("test-service");
+
+        when(payHubClient.updateCaseReference(
+            eq("test-service"), eq("exp-21321"), any(CaseReferenceRequest.class))
+        ).thenReturn(ResponseEntity.status(200).build());
+
+        messageHandler.updatePaymentCaseReferenceNew(updatePayment);
+
+        ArgumentCaptor<CaseReferenceRequest> requestCaptor = ArgumentCaptor.forClass(CaseReferenceRequest.class);
+        verify(payHubClient).updateCaseReference(any(), any(), requestCaptor.capture());
+
+        CaseReferenceRequest req = requestCaptor.getValue();
+        CaseReferenceRequest expectedRequest = new CaseReferenceRequest("cas-ref-9999");
+
+        assertThat(req).usingRecursiveComparison().isEqualTo(expectedRequest);
+    }
+
+    @Test
+    void should_throw_exception_if_payhub_api_update_payment_case_reference_fails() {
+        UpdatePayment updatePayment = new UpdatePayment(
+            "env-id-12321",
+            "Divorce",
+            "exp-21321",
+            "cas-ref-9999"
+        );
+
+        when(s2sTokenGenerator.generate()).thenReturn("test-service");
+
+        when(payHubClient.updateCaseReference(
+            eq("test-service"), eq("exp-21321"), any(CaseReferenceRequest.class))
+        ).thenThrow(mockFeignException);
+
+        PayHubCallException exception = catchThrowableOfType(
+            () -> messageHandler.updatePaymentCaseReferenceNew(updatePayment),
+            PayHubCallException.class
+        );
+
+        assertThat(exception.getMessage())
+            .isEqualTo(
+                "Failed updating payment. "
+                    + "Envelope id: env-id-12321, Exception record ref: exp-21321, New case ref: cas-ref-9999"
+            );
+        assertThat(exception.getCause()).isEqualTo(mockFeignException);
+    }
+
+    @Test
+    void should_call_payhub_api_and_ccd_api_for_successful_payment_creation() {
+        CreatePayment createPayment = new CreatePayment(
+            "envelope-id-123",
+            "ccd-case-ref-123",
+            true,
+            "po-box-123",
+            "Divorce",
+            "service",
+            Arrays.asList(new PaymentInfo("dcn-123"))
+        );
+
+        String s2sToken = "s2sToken1";
+
+        when(s2sTokenGenerator.generate()).thenReturn(s2sToken);
+
+        CreatePaymentRequest request = new CreatePaymentRequest(
+            "ccd-case-ref-123",
+            Arrays.asList("dcn-123"),
+            true,
+            "test-siteId"
+        );
+
+        when(requestMapper.mapPayments(createPayment)).thenReturn(request);
+        when(payHubClient.createPayment(any(), eq(request)))
+            .thenReturn(ResponseEntity.of(Optional.of(new CreatePaymentResponse(Arrays.asList("dcn-123")))));
+
+        messageHandler.handleCreatingPayment(createPayment);
+
+        verify(payHubClient).createPayment(s2sToken, request);
+        verify(ccdClient).completeAwaitingDcnProcessing(
+            "ccd-case-ref-123", "service", "Divorce"
+        );
+    }
+
+    @Test
+    void should_handle_conflict_exception_during_payment_creation() {
+        CreatePayment createPayment = new CreatePayment(
+            "envelope-id-123",
+            "ccd-case-ref-123",
+            true,
+            "po-box-123",
+            "Divorce",
+            "service",
+            Arrays.asList(new PaymentInfo("dcn-123"))
+        );
+
+        String s2sToken = "s2sToken1";
+
+        when(s2sTokenGenerator.generate()).thenReturn(s2sToken);
+
+        CreatePaymentRequest request = new CreatePaymentRequest(
+            "ccd-case-ref-123",
+            Arrays.asList("dcn-123"),
+            true,
+            "test-siteId"
+        );
+
+        when(requestMapper.mapPayments(createPayment)).thenReturn(request);
+        willThrow(FeignException.Conflict.class).given(payHubClient).createPayment(any(), any());
+
+        messageHandler.handleCreatingPayment(createPayment);
+
+        verify(payHubClient).createPayment(s2sToken, request);
+        verify(ccdClient).completeAwaitingDcnProcessing(
+            "ccd-case-ref-123", "service", "Divorce"
+        );
+    }
+
+    @Test
+    void should_throw_exception_when_payment_creation_fails_with_non_conflict_exception() {
+        CreatePayment createPayment = new CreatePayment(
+            "envelope-id-123",
+            "ccd-case-ref-123",
+            true,
+            "po-box-123",
+            "Divorce",
+            "service",
+            Arrays.asList(new PaymentInfo("dcn-123"))
+        );
+
+        String s2sToken = "s2sToken1";
+
+        when(s2sTokenGenerator.generate()).thenReturn(s2sToken);
+
+        CreatePaymentRequest request = new CreatePaymentRequest(
+            "ccd-case-ref-123",
+            Arrays.asList("dcn-123"),
+            true,
+            "test-siteId"
+        );
+
+        when(requestMapper.mapPayments(createPayment)).thenReturn(request);
+
+        FeignException exception = mock(FeignException.BadRequest.class);
+        doThrow(exception).when(payHubClient).createPayment(any(), any());
+
+        PayHubCallException ex = catchThrowableOfType(
+            () -> messageHandler.handleCreatingPayment(createPayment),
+            PayHubCallException.class
+        );
+
+        assertThat(ex.getMessage())
+            .isEqualTo("Failed creating payment. Envelope ID: envelope-id-123");
+        assertThat(ex.getCause()).isEqualTo(exception);
     }
 }
--- src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/PaymentRequestMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/payment/processor/service/PaymentRequestMapperTest.java
@@ -9,8 +9,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.client.payhub.request.CreatePaymentRequest;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.config.SiteConfiguration;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.errorhandling.exception.SiteNotFoundException;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.models.CreatePayment;
+import uk.gov.hmcts.reform.bulkscan.payment.processor.models.PaymentInfo;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.exceptions.InvalidMessageException;
 import uk.gov.hmcts.reform.bulkscan.payment.processor.service.servicebus.model.CreatePaymentMessage;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -88,5 +93,72 @@ class PaymentRequestMapperTest {
             () -> paymentRequestMapper.mapPaymentMessage(paymentMessage))
             .isInstanceOf(InvalidMessageException.class)
             .hasMessage("No Document Control Numbers found in the payment message. MessageId: envelope_id_123");
+    }
+
+    @Test
+    void should_return_valid_PaymentRequest_when_CreatePayment_is_valid() {
+        List<PaymentInfo> paymentInfos = Arrays.asList(
+            new PaymentInfo("DCN1"),
+            new PaymentInfo("DCN2")
+        );
+        CreatePayment createPayment = new CreatePayment(
+            "envelope_id_123",
+            "case_number_12345",
+            false,
+            PO_BOX,
+            "Divorce",
+            "finrem",
+            paymentInfos
+        );
+
+        when(siteConfig.getSiteIdByPoBox(PO_BOX)).thenReturn("A123");
+
+        CreatePaymentRequest expectedPaymentRequest = new CreatePaymentRequest(
+            "case_number_12345",
+            Arrays.asList("DCN1", "DCN2"),
+            false,
+            "A123"
+        );
+
+        CreatePaymentRequest paymentRequest = paymentRequestMapper.mapPayments(createPayment);
+        assertThat(paymentRequest).usingRecursiveComparison().isEqualTo(expectedPaymentRequest);
+    }
+
+    @Test
+    void should_throw_InvalidMessageException_when_no_payments_info_in_CreatePayment() {
+        CreatePayment createPayment = new CreatePayment(
+            "envelope_id_123",
+            "case_number_12345",
+            false,
+            PO_BOX,
+            "Divorce",
+            "finrem",
+            emptyList()
+        );
+
+        assertThatThrownBy(() -> paymentRequestMapper.mapPayments(createPayment))
+            .isInstanceOf(InvalidMessageException.class)
+            .hasMessage("No Document Control Numbers found in the payment message. MessageId: envelope_id_123");
+    }
+
+    @Test
+    void should_throw_SiteNotFoundException_when_site_not_found_for_poBox_in_CreatePayment() {
+        List<PaymentInfo> paymentInfos = Arrays.asList(
+            new PaymentInfo("DCN1")
+        );
+        CreatePayment createPayment = new CreatePayment(
+            "envelope_id_123",
+            "case_number_12345",
+            false,
+            PO_BOX,
+            "Divorce",
+            "finrem",
+            paymentInfos
+        );
+
+        when(siteConfig.getSiteIdByPoBox(PO_BOX)).thenReturn(null);
+        assertThatThrownBy(() -> paymentRequestMapper.mapPayments(createPayment))
+            .isInstanceOf(SiteNotFoundException.class)
+            .hasMessage("Site not Found for po box: " + PO_BOX);
     }
 }
